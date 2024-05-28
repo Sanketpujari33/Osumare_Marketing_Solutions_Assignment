b35494c7e9c20ba527f582c51658243902200f45
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-05-28 10:59:07 +0530

    complete  Osumare Marketing Solutions Assignment

--- backend/swagger.js
+++ backend/swagger.js
@@ -7,7 +7,7 @@ const options = {
         info: {
             title: 'Assignment: Todo List Management System',
             version: '1.0.0',
-            description: 'You are tasked with building a simple RESTful API using Node.js that manages a collection of tasks (e.g., to-do items). The API should allow users to perform basic CRUD (Create, Read, Update, Delete) operations on tasks. The data should be stored in memory (no database required). Your API should support the following endpoints: login: 'admin', password: 'admin''
+            description: 'You are tasked with building a simple RESTful API using Node.js that manages a collection of tasks (e.g., to-do items). The API should allow users to perform basic CRUD (Create, Read, Update, Delete) operations on tasks. The data should be stored in memory (no database required). Your API should support the following endpoints: login: admin, password: admin'
         },
         servers: [
             {
